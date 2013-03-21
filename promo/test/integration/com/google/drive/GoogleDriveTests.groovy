0d6c07dfc20bc422ee413de52cb553a88d0cce20
fsutin <federico.sutin@gmail.com>
2013-03-21 17:41:13 -0300

    Google drive beta test working

--- promo/test/integration/com/google/drive/GoogleDriveTests.groovy
+++ promo/test/integration/com/google/drive/GoogleDriveTests.groovy
@@ -3,30 +3,36 @@ package com.google.drive
 import static org.junit.Assert.*
 import org.junit.*
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.http.FileContent;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-//import com.google.api.client.json.jackson.JacksonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import org.springframework.aop.aspectj.RuntimeTestWalker.ThisInstanceOfResidueTestVisitor;
 
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.File;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+//import com.globallogic.sonar.exception.GoogleConnectionException
+import com.google.gdata.client.GoogleService.InvalidCredentialsException
+import com.google.gdata.client.spreadsheet.SpreadsheetService
+import com.google.gdata.data.spreadsheet.SpreadsheetEntry
+import com.google.gdata.data.spreadsheet.SpreadsheetFeed
 
 class GoogleDriveTests {
 
-	private static String CLIENT_ID = "YOUR_CLIENT_ID";
-	private static String CLIENT_SECRET = "YOUR_CLIENT_SECRET";
-	private static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+//	private static String CLIENT_ID = "743045243915.apps.googleusercontent.com";
+//	private static String CLIENT_SECRET = "zxeqm_w1LYGJVVsk9KT89Q2Q";
+//	private static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+	
+	/** The URL for spread sheets*/
+	String SPREADSHEET_FEED_URL = "https://spreadsheets.google.com/feeds/spreadsheets/private/full"
+	
+	/** The name of the spreadsheet service */
+	String SPREADSHEET_SERVICE = "TheSpreadsheetService"
+	
+	/** The spring security service injection */
+	def springSecurityService
+	
+	/** The google spreadsheet service */
+	def googleSpreadsheetService
+	
+	/**
+	 * Authenticates into the google
+	 * drive service
+	 */
 	
     @Before
     void setUp() {
@@ -40,38 +46,51 @@ class GoogleDriveTests {
 
     @Test
     void testSomething() {
-    HttpTransport httpTransport = new NetHttpTransport();
-	JsonFactory jsonFactory = new JacksonFactory();
-   
-	GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-		httpTransport, jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(DriveScopes.DRIVE))
-		.setAccessType("online")
-		.setApprovalPrompt("auto").build();
-	
-	String url = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
-	System.out.println("Please open the following URL in your browser then type the authorization code:");
-	System.out.println("  " + url);
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	String code = br.readLine();
-	
-	GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
-	GoogleCredential credential = new GoogleCredential().setFromTokenResponse(response);
-	
-	//Create a new authorized API client
-	Drive service = new Drive.Builder(httpTransport, jsonFactory, credential).build();
-
-	//Insert a file
-	File body = new File();
-	body.setTitle("My document");
-	body.setDescription("A test document");
-	body.setMimeType("text/plain");
-	
-	java.io.File fileContent = new java.io.File("document.txt");
-	FileContent mediaContent = new FileContent("text/plain", fileContent);
-
-	File file = service.files().insert(body, mediaContent).execute();
-	System.out.println("File ID: " + file.getId());
-		assert 1==1
+		def result =  listSpreadSheets()
+		assert result != null
     }
+	
+	/**
+	 * Authenticates into the google
+	 * drive service
+	 */
+	def authenticate(def service){
+//		GoogleCredentials credentials = User.get(springSecurityService.getPrincipal().id).googleCredentials
+		try {
+			service.setUserCredentials('federico.sutin@globallogic.com', '');
+		} catch (InvalidCredentialsException e) {
+			throw new IllegalAccessError(e.getMessage());
+		}
+	}
+	
+	/**
+	 * @return the spread sheet service
+	 */
+	def getSpreadsheetService(){
+		if(this.googleSpreadsheetService == null) {
+			this.googleSpreadsheetService = new SpreadsheetService(this.SPREADSHEET_SERVICE)
+		}
+		this.authenticate(this.googleSpreadsheetService)
+		return this.googleSpreadsheetService
+	}
+	
+	/**
+	 * @return a list of spreadsheets
+	 */
+	def listSpreadSheets(){
+		URL url = new URL(this.SPREADSHEET_FEED_URL);
+		SpreadsheetFeed feed = this.getSpreadsheetService().getFeed(url, SpreadsheetFeed.class);
+		
+//		SpreadsheetsService spreedSheetService = new SpreadsheetsService("MySpreadsheetIntegration-v1");
+		this.getSpreadsheetService().RequestFactory
+		
+		
+//		List<SpreadsheetEntry> spreadsheets = feed.getEntries();
+//		def result = [:]
+//		for (SpreadsheetEntry sheet : spreadsheets) {
+//			result.put(sheet.getTitle().getPlainText(), sheet.getEditLink()?.getHref())
+//		}
+		return result
+	}
 }
 
