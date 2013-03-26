b5286a16fed2d88e3393220530ad5b0b070507e4
fsutin <federico.sutin@gmail.com>
2013-03-26 00:06:20 -0300

    Fixed SonarExtractorService

--- promo/grails-app/services/org/sonar/SonarExtractorService.groovy
+++ promo/grails-app/services/org/sonar/SonarExtractorService.groovy
@@ -1,14 +1,45 @@
 package org.sonar
 
 import org.sonar.wsclient.Sonar
+import org.sonar.wsclient.services.MetricQuery;
 import org.sonar.wsclient.services.Resource
 import org.sonar.wsclient.services.ResourceQuery;
+import org.springframework.context.support.StaticApplicationContext;
 
 class SonarExtractorService {
+	
+	private static Sonar instance
 
-    def extractApacheMavenRCIMeasure() {
-		Sonar sonar = Sonar.create("https://analysis.apache.org/")
-		Resource apacheMaven = sonar.find(ResourceQuery.createForMetrics("org.apache.maven:maven", "coverage", "lines", "violations"));
-		return apacheMaven.getMeasure("coverage");
+    private Sonar getSonarInstance() {
+		if(instance == null)
+		 instance = Sonar.create("https://analysis.apache.org/")
+		 //instance = Sonar.create(url, user, pass)
+		return instance
     }
+	
+	def getSonarResources(){
+		return getSonarInstance().findAll(new ResourceQuery())
+	}
+	
+	def getSonarMetrics(){
+		return getSonarInstance().findAll(new MetricQuery())
+	}
+	
+	def getResource(Long resourceId){
+		return getSonarInstance().find(new ResourceQuery().setResourceId(resourceId))
+	}
+	
+	def getResource(String resourceKey){
+		return getSonarInstance().find(new ResourceQuery().setResourceKeyOrId(resourceKey))
+	}
+	
+	def getMetricFromResource(String resourceKey,String metric){
+		return getSonarInstance().find(ResourceQuery.createForMetrics(resourceKey,metric))
+	}
+	
+	def getMetricFromAllResources(String metric){
+		return getSonarInstance().findAll(new ResourceQuery().setMetrics(metric))
+	}
+	
+	
 }
